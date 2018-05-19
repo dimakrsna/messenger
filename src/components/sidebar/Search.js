fdada71dd533f5056978542598df0e4e28d9f349
dimakrsna <dimakrsna@gmail.com>
2018-05-19 22:18:39 +0300

    Возврат отфильтрованного масива фильтра

--- src/components/sidebar/Search.js
+++ src/components/sidebar/Search.js
@@ -4,14 +4,26 @@ import {mainData} from "./../../data/mainData";
 export class Search extends React.Component {
     constructor(props){
         super(props);
-        this.state = {mainData: mainData};
+        this.state = {
+            mainData: mainData,
+            // filteredData: []
+        };
     }
 
-    test(event, state) {
-        let value = event.target.value;
+    filterContactList(event, state) {
+        let value = event.target.value,
+            filtered;
 
-        console.log(value);
-        console.log(state);
+        // console.log(value);
+        // console.log(state);
+
+        filtered = state.mainData.filter(function(item){
+                return item.name.toLowerCase().search(value) !== -1;
+            });
+
+        console.log(filtered);
+
+        // this.setState({filteredData: filtered});
     }
 
     render() {
@@ -19,7 +31,7 @@ export class Search extends React.Component {
             <div className="search">
                 <label className="search__icon" htmlFor="searchInput"></label>
                 <input type="text" placeholder="CLIENTS" id="searchInput" className="search__input"
-                       onChange={(event) => ( this.test(event, this.state) )}/>
+                       onChange={(event) => ( this.filterContactList(event, this.state) )}/>
             </div>
         )
     }
